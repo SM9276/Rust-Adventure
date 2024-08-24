9f0c71c90b03cfa824bf57c97ea03bfcd366431e
Sergio Mercado Nuñez <sm9276@g.rit.edu>
2024-08-24 01:10:32 -0400

    Chapter 1 Complete

---
+++ Chapter2/target/debug/deps/Chapter2-1695354e5ea2874c.d
@@ -0,0 +1,5 @@
+/home/mangoblop/Documents/GitHub/Rust-Adventure/Chapter2/target/debug/deps/libChapter2-1695354e5ea2874c.rmeta: src/main.rs
+
+/home/mangoblop/Documents/GitHub/Rust-Adventure/Chapter2/target/debug/deps/Chapter2-1695354e5ea2874c.d: src/main.rs
+
+src/main.rs:
---
+++ Chapter2/target/debug/deps/Chapter2-41cabe539a0de6b5.d
@@ -0,0 +1,5 @@
+/home/mangoblop/Documents/GitHub/Rust-Adventure/Chapter2/target/debug/deps/libChapter2-41cabe539a0de6b5.rmeta: src/main.rs
+
+/home/mangoblop/Documents/GitHub/Rust-Adventure/Chapter2/target/debug/deps/Chapter2-41cabe539a0de6b5.d: src/main.rs
+
+src/main.rs:
