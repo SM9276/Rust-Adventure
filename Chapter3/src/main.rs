295038d8df14cf0f31d4d4720db984171f402925
Sergio Mercado Nuñez <sm9276@g.rit.edu>
2024-08-27 12:27:49 -0400

    Chapter 3 WIP

--- Chapter3/src/main.rs
+++ Chapter3/src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    //Variables
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}"); 
+    
+    //Shawdowing
+    let y = 6;
+
+    let y = y + 1;
+
+    {
+        let y = y *2;
+        println!("The value of y in the inner scope is: {y}");
+    }
+    
+    println!("The value of y is: {y}");
+    
+    let spaces = "   ";
+    let spaces = spaces.len();
+    //Reassigning the name instead of reassigning the variable 
 }
